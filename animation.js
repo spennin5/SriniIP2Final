a83cb42c48834c421b0044c3ff78a3561d65ddfe
Rayianna Daniels <>
2021-07-19 15:38:20 -0400

    Added style, updated HTML with some classes for said style, and added a very very very basic animation.

---
+++ animation.js
@@ -0,0 +1,7 @@
+document.getElementById('submit').addEventListener('click', function() {
+    // moveUp animation
+    document.getElementById('mainContent').classList.add('transitionAnimation');
+    
+    // loading animation
+    //document.getElementById('')
+})
